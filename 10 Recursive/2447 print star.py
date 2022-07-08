eb7bafda8156921d34a8eb13b483606cb868adc3
Dasom Ahn <lizzy0521@naver.com>
2022-07-08 15:19:37 +0900

    2477

---
+++ 10 Recursive/2447 print star.py
@@ -0,0 +1,27 @@
+# num = int(input())
+
+
+# def star(arr, n):
+#     arr[0] = "*"*3
+#     arr[1] = "* *"
+#     arr[2] = "*"*3
+
+#     if n == 3:
+#         printstar(arr)
+    
+#     else:
+#         star(string, n//3)
+
+# star("*", num)
+
+# def printstar(arr):
+arr=[]
+arr[0][0] = "1"
+arr[0][1] = "11"
+arr[0][2] = "3"
+arr[1][0] = "11"
+arr[0][1] = "1111"
+arr[0][2] = "21"
+arr[2][0] = "1"
+arr[2][1] = "1"
+arr[2][2] = "1"
