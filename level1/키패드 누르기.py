631ee3231a15f38fb6e6b824da792461af6f7766
Dasom Ahn <lizzy0521@naver.com>
2022-07-13 03:26:00 +0900

    https://blog.naver.com/idonttell/222809187510

--- level1/키패드 누르기.py
+++ level1/키패드 누르기.py
@@ -1,29 +1,39 @@
-def dist(cur, num):
-    dist = 0
-    cur = cur if cur != 0 else 11
-    num = num if num != 0 else 11
-
-    ab = abs(cur-num)
-
-    if (ab%3) == 0:
-        dist = ab/3
-    else:
-        if ab == 1: 
-            dist = 1
-        elif ab in [2, 4]:
-            dist = 2
-        elif ab in [5, 7]:
-            dist = 3
-        else:
-            dist = 4
-
-    return dist
-
+# comments are my first try
+# def dist(cur, num):
+#     cur = cur if cur != 0 else 11
+#     num = num if num != 0 else 11
+#     ab = abs(cur-num)
+    
+#     if (ab%3) == 0:
+#         dist = ab/3
+#     else:
+#         if ab == 1: 
+#             dist = 1
+#         elif ab in [2, 4]:
+#             dist = 2
+#         elif ab in [5, 7]:
+#             dist = 3
+#         else:
+#             dist = 4
+    
+#     return dist
+    
 def solution(numbers, hand):
     answer = ''
-    left = 10
-    right = 12
-
+    
+    # different way
+    dic = {
+        1:(0, 0), 2:(0, 1), 3:(0, 2),
+        4:(1, 0), 5:(1, 1), 6:(1, 2),
+        7:(2, 0), 8:(2, 1), 9:(2, 2),
+        '*':(3, 0), 0:(3, 1), '#':(3, 2)
+          }
+    
+    # left = 10
+    # right = 12
+    left = '*'
+    right = '#'
+    
     for num in numbers:
         if num in [1, 4, 7]:
             answer += "L"
@@ -32,16 +42,16 @@ def solution(numbers, hand):
             answer += "R"
             right = num
         else:
-            rightdist = dist(right, num)
-            leftdist = dist(left, num)
-
-            print(num)
-            print(left, right)
-            print(leftdist, rightdist)
-            if rightdist > leftdist:
+            # rightdist = dist(right, num)
+            # leftdist = dist(left, num)
+            
+            rdist = abs(dic[num][0]-dic[right][0]) + abs(dic[num][1]-dic[right][1])
+            ldist = abs(dic[num][0]-dic[left][0]) + abs(dic[num][1]-dic[left][1])
+            
+            if rdist > ldist:
                 answer += "L"
                 left = num
-            elif rightdist < leftdist:
+            elif rdist < ldist:
                 answer += "R"
                 right = num
             else:
@@ -51,5 +61,5 @@ def solution(numbers, hand):
                 else:
                     answer += "L"
                     left = num
-
+                    
     return answer
