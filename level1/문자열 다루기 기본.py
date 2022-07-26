7cf6838771a8314f5362eef69ec425772ba44aed
Dasom Ahn <lizzy0521@naver.com>
2022-07-26 21:59:27 +0900

    unfinished

---
+++ level1/문자열 다루기 기본.py
@@ -0,0 +1,19 @@
+# import re
+
+def solution(s):
+    
+    # return False if re.search("[\D]", s) else True
+    
+    # for i in s:
+    #     if i not in (range(10)):
+    #         return False
+    # return True
+
+
+    # try:
+    #     int(s)
+    # except ValueError:
+    #     return False
+    # return True
+    
+    # return True if s.isdecimal() else False
