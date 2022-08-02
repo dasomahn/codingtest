8715fd9ff866c6fc1e4b2766094140350efa8433
Dasom Ahn <lizzy0521@naver.com>
2022-08-02 18:51:28 +0900

    unfinished

---
+++ 가장 큰 수.py
@@ -0,0 +1,15 @@
+def solution(numbers):
+#     made = []
+    
+#     for i in range(len(numbers)-1):
+#         for j in range(i+1, len(numbers)):
+#             numbers[i], numbers[j] = numbers[j], numbers[i]
+#             made.append("".join(str(i) for i in numbers))
+#             made.append("".join(str(i) for i in reversed(numbers)))
+            
+#             numbers[i], numbers[j] = numbers[j], numbers[i]
+            
+#     return str(max(made))
+
+                    
+        
