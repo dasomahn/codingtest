d846d9475905e169f79c86af12812960c9ac44f5
Dasom Ahn <lizzy0521@naver.com>
2022-08-16 02:28:13 +0900

    https://blog.naver.com/idonttell/222849303267 - edited

--- [완전탐색] 피로도.py
+++ [완전탐색] 피로도.py
@@ -1,23 +1,43 @@
-from itertools import permutations        
+# from itertools import permutations        
+
+# def solution(k, dungeons):
+
+#     per = permutations(dungeons, len(dungeons))
+#     rounds = []
+#     life = k
+    
+#     for case in per:
+#         answer = 0
+#         k = life
+#         for d in case:
+#             if k >= d[0]:
+#                 answer += 1
+#                 k -= d[1]
+#             else:
+#                 rounds.append(answer)
+#                 break
+#         rounds.append(answer)
+    
+#     return max(rounds)
+
+# DFS solution
+answer = 0
 
 def solution(k, dungeons):
-
-    per = permutations(dungeons, len(dungeons))
-    rounds = []
-    life = k
     
-    for case in per:
-        answer = 0
-        k = life
-        for d in case:
-            if k >= d[0]:
-                answer += 1
-                k -= d[1]
-            else:
-                rounds.append(answer)
-                break
-        rounds.append(answer)
+    N = len(dungeons)
+    visited = [0] * N
     
-    return max(rounds)
+    def dfs(k, cnt):
+        global answer
+        if cnt > answer:
+            answer = cnt
 
-# solution = lambda k, d: max([solution(k - u, d[:i] + d[i+1:]) + 1 for i, (m, u) in enumerate(d) if k >= m] or [0])
+        for j in range(N):
+            if k >= dungeons[j][0] and not visited[j]:
+                visited[j] = 1
+                dfs(k - dungeons[j][1], cnt + 1)
+                visited[j] = 0                
+                
+    dfs(k, 0)
+    return answer
