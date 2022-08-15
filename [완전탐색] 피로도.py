b95b488256077c4969dc498bd8071bd7263866b1
Dasom Ahn <lizzy0521@naver.com>
2022-08-15 23:50:43 +0900

    https://blog.naver.com/idonttell/222849303267

--- [완전탐색] 피로도.py
+++ [완전탐색] 피로도.py
@@ -19,3 +19,5 @@ def solution(k, dungeons):
         rounds.append(answer)
     
     return max(rounds)
+
+# solution = lambda k, d: max([solution(k - u, d[:i] + d[i+1:]) + 1 for i, (m, u) in enumerate(d) if k >= m] or [0])
