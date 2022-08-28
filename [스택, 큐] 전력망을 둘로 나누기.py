a080c858212f17da3f48134b6187d70e3445e891
Dasom Ahn <lizzy0521@naver.com>
2022-08-28 21:18:58 +0900

    https://blog.naver.com/idonttell/222867982892 - edited

--- [스택, 큐] 전력망을 둘로 나누기.py
+++ [스택, 큐] 전력망을 둘로 나누기.py
@@ -33,3 +33,33 @@ def solution(n, wires):
         tree[b].append(a)
     
     return answer
+
+# Union-find Algorithm
+def getParent(parent, i):
+    if i == parent[i-1]:
+        return i
+    return getParent(parent, parent[i-1])
+
+def unionParent(parent, a, b):
+    a = getParent(parent, a)
+    b = getParent(parent, b)
+    parent[max(a, b)-1] = min(a, b)
+
+def solution(n, wires):
+    answer = n - 2
+    if n <= 3:
+        return answer
+    
+    parent = []
+    for cnt in range(n-1):
+        parent = [i for i in range(1, n+1)]
+
+        for a, b in wires[:cnt] + wires[cnt+1:]:
+            unionParent(parent, a, b)
+    
+        for i in range(n):
+            parent[i] = getParent(parent, i+1)
+        
+        answer = min(answer, abs(2*parent.count(1)-n))
+    
+    return answer  
