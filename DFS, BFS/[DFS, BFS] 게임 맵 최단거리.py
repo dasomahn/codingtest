f14ad1f801bcff090a3f9b973efe8e546fdbdd93
Dasom Ahn <lizzy0521@naver.com>
2022-09-16 05:11:44 +0900

    https://blog.naver.com/idonttell/222875898326

--- DFS, BFS/[DFS, BFS] 게임 맵 최단거리.py
+++ DFS, BFS/[DFS, BFS] 게임 맵 최단거리.py
@@ -1,64 +1,24 @@
-answer = 25
-
 def solution(maps):
     direction = [(0, 1), (1, 0), (-1, 0), (0, -1)]
     n = len(maps)
     m = len(maps[0])
     
-    def bfs(x, y):
-        visit = []
-        queue = []
-        cnt = 0
-        
-        queue.append((x, y))
-        
-        while queue:
-            nx, ny = queue.pop(0)
-            if (nx, ny) == (0, 0):
-                return answer
-            
-            if nx in [-1, n] or ny in [-1, m] or (nx, ny) in visit or maps[nx][ny]==0:
-                print("stp", nx, ny, queue)
-                continue
-            
-            print(nx, ny, queue)
-            visit.append((nx, ny))
-            for dx, dy in direction:
-                queue.append((x+dx, y+dy))
-        
-        return -1
-            
-    bfs(n-1, m-1)
-        
+    visit = [[0 for _ in range(m)] for _ in range(n)]
+    queue = []
 
-'''
-answer = 25
+    queue.append((n-1, m-1, 1))
 
-def solution(maps):
-    direction = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-    n = len(maps)
-    m = len(maps[0])
-    
-    def move(x, y, visit, cnt):
-        global answer
+    while queue:
+        x, y, cnt = queue.pop(0)
         
-        if x==0 and y==0:
-            print("arr")
-            answer = min(answer, cnt)
-            return
-        
-        if x in [-1, n] or y in [-1, m] or maps[x][y]==0 or (x,y) in visit:
-            print("stp", x, y, visit)
-            return
-        
-        visit.append((x, y))
+        if (x, y) == (0, 0):
+            return cnt
 
-        print(x, y, cnt)
+        if x in [-1, n] or y in [-1, m] or visit[x][y]==1 or maps[x][y]==0:
+            continue
+
+        visit[x][y] = 1
         for dx, dy in direction:
-            move(x+dx, y+dy, visit, cnt+1)
-            
-    # visit = [[0]*m]*n
-    move(n-1, m-1, [], 1)
-        
-    return answer if answer != 25 else -1
-'''
+            queue.append((x+dx, y+dy, cnt+1))
+
+    return -1
