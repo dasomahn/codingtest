5afabc4523639e66f3ac8ddae840b475e54efb84
Dasom Ahn <lizzy0521@naver.com>
2023-01-06 23:45:29 +0900

    20230106

--- 탐욕법/[탐욕법] 조이스틱.py
+++ 탐욕법/[탐욕법] 조이스틱.py
@@ -1,39 +1,66 @@
-length = 0
+answer = 0
 
 def mv_alpha(c):
     return min(c-65, 91-c)
 
+def getNear(idx):
+    left = 1
+    while (alp[(idx-left) % length] == 0):
+        left += 1
+        
+    right = 1
+    while (alp[(idx+right) % length] == 0):
+        right += 1
+    
+    return (left, right)
+
+def dfs(idx, dist, dir, turnCnt):
+    global answer
+    print(alp, idx, dist, dir, turnCnt)
+
+    temp = alp[idx]
+    alp[idx] = 0
+    
+    if sum(alp) == 0:
+        answer = dist
+        return 1
+
+    left, right = getNear(idx)
+    if (left < right):
+        mv, nxtDir, nxtIdx = left, "left", (idx-left)%length
+    else:
+        mv, nxtDir, nxtIdx = right, "right", (idx+right)%length
+    # print(nxtDir)
+    tempTurnCnt = turnCnt
+    turnCnt += 0 if dir == nxtDir else 1
+    if (turnCnt <= 2 and dfs(nxtIdx, dist+mv, nxtDir, turnCnt)):
+        return 1
+    
+    # 해당 방향이 아닐 때
+    if (left > right):
+        mv, nxtDir, nxtIdx = left, "left", (idx-left)%length
+    else:
+        mv, nxtDir, nxtIdx = right, "right", (idx+right)%length
+    
+    turnCnt = tempTurnCnt if dir == nxtDir else tempTurnCnt + 1
+    if (turnCnt <= 2 and dfs(nxtIdx, dist+mv, nxtDir, turnCnt)):
+        return 1
+
+    alp[idx] = temp
+    turnCnt = tempTurnCnt
+    return 0
+
 def solution(name):
-    global length
+    global length, alp, answer
     length = len(name)
     
-    moves = [0] * length
+    if "A"*length == name:
+        return 0
     
+    alp = [0] * length
     for idx, c in enumerate(name):
-        moves[idx] = mv_alpha(ord(c))
+        alp[idx] = mv_alpha(ord(c))
     
-    answer = 0
-    cur = 0
-    while any(m != 0 for m in moves):
-        answer += moves[cur]
-        moves[cur] = 0
-        
-        if all(m == 0 for m in moves):
-            return answer
-        
-        left = 1
-        while (moves[(cur-left) % length] == 0):
-            left += 1
-            
-        right = 1
-        while (moves[(cur+right) % length] == 0):
-            right += 1
-        print(cur, moves, left, right)
-        if (left < right):
-            cur = (cur - left) % length
-        else:
-            cur = (cur + right) % length
-
-        answer += min(left, right)
-        
+    dfs(0, sum(alp), None, 0)
+    
     return answer
