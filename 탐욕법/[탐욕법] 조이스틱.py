ae12404d5349332793307108bd921ed074c1b4bc
Dasom Ahn <lizzy0521@naver.com>
2023-01-07 14:03:53 +0900

    20230107

--- 탐욕법/[탐욕법] 조이스틱.py
+++ 탐욕법/[탐욕법] 조이스틱.py
@@ -1,66 +1,21 @@
-answer = 0
-
-def mv_alpha(c):
-    return min(c-65, 91-c)
-
-def getNear(idx):
-    left = 1
-    while (alp[(idx-left) % length] == 0):
-        left += 1
-        
-    right = 1
-    while (alp[(idx+right) % length] == 0):
-        right += 1
-    
-    return (left, right)
-
-def dfs(idx, dist, dir, turnCnt):
-    global answer
-    print(alp, idx, dist, dir, turnCnt)
-
-    temp = alp[idx]
-    alp[idx] = 0
-    
-    if sum(alp) == 0:
-        answer = dist
-        return 1
-
-    left, right = getNear(idx)
-    if (left < right):
-        mv, nxtDir, nxtIdx = left, "left", (idx-left)%length
-    else:
-        mv, nxtDir, nxtIdx = right, "right", (idx+right)%length
-    # print(nxtDir)
-    tempTurnCnt = turnCnt
-    turnCnt += 0 if dir == nxtDir else 1
-    if (turnCnt <= 2 and dfs(nxtIdx, dist+mv, nxtDir, turnCnt)):
-        return 1
-    
-    # 해당 방향이 아닐 때
-    if (left > right):
-        mv, nxtDir, nxtIdx = left, "left", (idx-left)%length
-    else:
-        mv, nxtDir, nxtIdx = right, "right", (idx+right)%length
-    
-    turnCnt = tempTurnCnt if dir == nxtDir else tempTurnCnt + 1
-    if (turnCnt <= 2 and dfs(nxtIdx, dist+mv, nxtDir, turnCnt)):
-        return 1
-
-    alp[idx] = temp
-    turnCnt = tempTurnCnt
-    return 0
+def rotate(list, n):
+    return list[n:] + list[:n]
 
 def solution(name):
-    global length, alp, answer
-    length = len(name)
-    
-    if "A"*length == name:
+    size = len(name)
+    if name == "A"*size:
         return 0
     
-    alp = [0] * length
-    for idx, c in enumerate(name):
-        alp[idx] = mv_alpha(ord(c))
+    answer = size
+    for i in range(size//2):
+        clock = rotate(name, -i)
+        c_clock = rotate(name, i+1)[::-1]
+
+        for lst in [clock, c_clock]:
+            moves = size-1
+            while(moves >= 0 and lst[moves] == 'A'):
+                moves -= 1
+
+            answer = min(answer, moves+i)
     
-    dfs(0, sum(alp), None, 0)
-    
-    return answer
+    return answer + sum(min(n-65, 91-n) for n in map(ord, name))
