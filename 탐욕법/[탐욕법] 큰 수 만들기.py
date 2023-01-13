01b91194e894847ca494f3bd58f1353557f59406
Dasom Ahn <lizzy0521@naver.com>
2023-01-14 03:24:21 +0900

    20230114

--- 탐욕법/[탐욕법] 큰 수 만들기.py
+++ 탐욕법/[탐욕법] 큰 수 만들기.py
@@ -1,3 +1,4 @@
+'''
 def remove(string, idx):
     return string[:idx] + string[idx+1:]
 
@@ -26,10 +27,26 @@ def solution(number, k):
 
         while (k > 0) and (idx != max_idx):
             min_idx = get_min(number, idx, max_idx)
-            number = remove(number, min_idx)
+            number = remove(number, min_idx) # min index로 지우기
             k -= 1
             max_idx -= 1
 
         idx = max_idx+1
 
     return number
+'''
+
+# fixed
+def solution(number, k):
+    stack = []
+    
+    for n in number:
+        while (stack) and (stack[-1] < n) and (k > 0):
+            stack.pop()
+            k -= 1
+        stack.append(n)
+    
+    if k != 0:
+        return number[:-k]
+
+    return ''.join(stack)
