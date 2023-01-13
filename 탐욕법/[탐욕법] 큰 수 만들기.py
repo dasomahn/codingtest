a02a8a41c47574a8c6b25a5c525c8da0745d9214
Dasom Ahn <lizzy0521@naver.com>
2023-01-13 17:02:47 +0900

    20230113
    TC 11/12

--- 탐욕법/[탐욕법] 큰 수 만들기.py
+++ 탐욕법/[탐욕법] 큰 수 만들기.py
@@ -1,19 +1,16 @@
 def solution(number, k):
-    answer = ''
     lst = [n for n in number]
-    
-    init = len(lst)
-    
-    i = 0
-    while (i < len(lst) - 1) :
-        if len(lst) + k == init:
-            break
+
+    idx = 0
+    while (k > 0):
+        max_temp = lst[idx:idx+k+2]
+        max_idx = idx + max_temp.index(max(max_temp))
+
+        while (k > 0) and (idx != max_idx):
+            min_temp = lst[idx:max_idx]
+            del lst[idx + min_temp.index(min(min_temp))] # min index로 지우기
+            k -= 1
+            max_idx -= 1      
+        idx = max_idx+1
         
-        if lst[i] < lst[i+1]:
-            lst.remove(lst[i])
-        elif lst[i] > lst[i+1]:
-            lst.remove(lst[i+1])
-        
-        i += 1
-    
     return ''.join(lst)
