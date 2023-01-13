825477cb332a38aa12ec3673af7611810d43e694
Dasom Ahn <lizzy0521@naver.com>
2023-01-13 18:27:56 +0900

    20230113

--- 탐욕법/[탐욕법] 큰 수 만들기.py
+++ 탐욕법/[탐욕법] 큰 수 만들기.py
@@ -1,16 +1,35 @@
-def solution(number, k):
-    lst = [n for n in number]
+def remove(string, idx):
+    return string[:idx] + string[idx+1:]
 
+def get_min(string, start, end):
+    num = "0123456789"
+    for n in num:
+        for i in range(start, end):
+            if n == string[i]:
+                return i
+
+def get_max(string, start, end):
+    num = "9876543210"
+    for n in num:
+        for i in range(start, end):
+            if n == string[i]:
+                return i
+
+def solution(number, k):
     idx = 0
     while (k > 0):
-        max_temp = lst[idx:idx+k+2]
-        max_idx = idx + max_temp.index(max(max_temp))
+        if (idx == len(number)-k):
+            number = number[:-k]
+            break
+
+        max_idx = get_max(number, idx, idx+k+1)
 
         while (k > 0) and (idx != max_idx):
-            min_temp = lst[idx:max_idx]
-            del lst[idx + min_temp.index(min(min_temp))] # min index로 지우기
+            min_idx = get_min(number, idx, max_idx)
+            number = remove(number, min_idx)
             k -= 1
-            max_idx -= 1      
+            max_idx -= 1
+
         idx = max_idx+1
-        
-    return ''.join(lst)
+
+    return number
