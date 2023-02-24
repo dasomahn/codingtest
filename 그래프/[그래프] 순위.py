603f72af242b7c41e851bd4d8bd45a107430ed40
Dasom Ahn <lizzy0521@naver.com>
2023-02-24 11:36:28 +0900

    20230224 - 순위
    floyd-warshall ver.

--- 그래프/[그래프] 순위.py
+++ 그래프/[그래프] 순위.py
@@ -52,3 +52,29 @@ def solution(n, results):
             answer += 1
     
     return answer
+
+# floyd-warshall
+def solution(n, results):
+    board = [[0] * n for _ in range(n)]
+    
+    # p1 > p2는 1, p1 < p2는 -1로 초기화
+    for p1, p2 in results:
+        board[p1 - 1][p2 - 1] = 1       # win
+        board[p2 - 1][p1 - 1] = -1      # lose
+
+    for k in range(n): # k: 경유점
+        for i in range(n):
+            for j in range(n):
+                if board[i][k] == 1 and board[k][j] == 1:
+                    board[i][j] = 1     # 만약 i > k이고, k > j라면 i > j
+                elif board[i][k] == -1 and board[k][j] == -1:
+                    board[i][j] = -1    # 만약 i < k이고, k < j라면 i < j
+
+    # 각 노드 점수판에 0이 하나인 경우 판별
+    # 즉, 다른 노드들에 대해 승패가 모두 결정되었다는 의미
+    answer = 0
+    for i in range(n):
+        if board[i].count(0) == 1:
+            answer += 1
+
+    return answer
