1c7dcdc6d1e00d83afb6cbb5a056d03468e5566d
Dasom Ahn <lizzy0521@naver.com>
2023-02-24 14:21:26 +0900

    20230224 - N으로 표현
    update

--- 동적계획법/N으로 표현.py
+++ 동적계획법/N으로 표현.py
@@ -5,7 +5,8 @@
 # ...
 # nn...nn, 1 op N-1, 2 op N-2, ... N-1, 1
 
-def four_op(a_lst, b_lst, s):
+def four_op(a_lst, b_lst):
+    s = []
     for a in a_lst:
         for b in b_lst:
             s.append(a + b)
@@ -15,30 +16,26 @@ def four_op(a_lst, b_lst, s):
                     s.append(a - b)
                 if b != 0:
                     s.append(a // b)
+    return s
     
 def solution(N, number):
-    answer = 1
     repeat = 0
     case = {}
     
-    while (answer <= 8):
+    for answer in range(1, 9):
         cases = []
         
         repeat = repeat * 10 + N
-        if (repeat == number):
-            return answer
-        else:
-            cases.append(repeat)
+        if (repeat == number): return answer
+        cases.append(repeat)
         
         # 사칙연산        
         for i in range(1, answer):
-            four_op(case[i], case[answer-i], cases)
-
-        set_cases = set(cases)
-        if (number in set_cases):
-            return answer
+            s = four_op(case[i], case[answer-i])
+            
+            if (number in s): return answer
+            cases.extend(s)
         
-        case[answer] = set_cases
-        answer += 1
+        case[answer] = set(cases)
     
     return -1
