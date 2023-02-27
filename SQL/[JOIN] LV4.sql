8b8ababcf481f30a6d359470406eaa1c9b6c9ad1
Dasom Ahn <lizzy0521@naver.com>
2023-02-27 20:51:11 +0900

    20230227

--- SQL/[JOIN] LV4.sql
+++ SQL/[JOIN] LV4.sql
@@ -24,3 +24,30 @@ JOIN FOOD_ORDER as o on p.product_id = o.product_id
 WHERE YEAR(PRODUCE_DATE) = 2022 and MONTH(PRODUCE_DATE) = 5
 GROUP BY p.product_id
 ORDER BY TOTAL_PRICE DESC, p.product_id
+
+-- 주문량이 많은 아이스크림들 조회하기
+SELECT j.flavor
+FROM FIRST_HALF as f
+JOIN JULY as j on f.flavor = j.flavor
+GROUP BY j.flavor
+ORDER BY j.total_order + SUM(f.total_order) DESC
+LIMIT 3
+
+-- 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기
+SELECT *
+FROM (
+    SELECT CAR_ID, c.CAR_TYPE, FLOOR(daily_fee * (100 - discount_rate) * 0.3) as FEE
+    FROM CAR_RENTAL_COMPANY_CAR as c
+    JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p on c.car_type = p.car_type
+    WHERE car_id not in (
+            SELECT car_id
+            FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+            WHERE (start_date between '2022-11-01' and '2022-11-30')
+                or (end_date between '2022-11-01' and '2022-11-30')
+                or (start_date < '2022-11-01' and '2022-11-30' < end_date)
+        )
+        and c.car_type in ('세단', 'SUV')
+        and duration_type = '30일 이상'
+) as t
+WHERE 500000 <= fee and fee < 2000000
+ORDER BY fee DESC, car_type, car_id DESC
