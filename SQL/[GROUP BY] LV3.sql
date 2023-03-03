e42e720392a6a5234914907bfce1d17c88d0a650
Dasom Ahn <lizzy0521@naver.com>
2023-03-03 23:18:14 +0900

    20230303

--- SQL/[GROUP BY] LV3.sql
+++ SQL/[GROUP BY] LV3.sql
@@ -26,3 +26,38 @@ FROM (
 ) as T
 WHERE TOTAL_RECORDS >= 5
 ORDER BY MONTH, car_id DESC
+
+-- 카테고리 별 도서 판매량 집계하기
+SELECT category, SUM(sales) as TOTAL_SALES
+FROM BOOK
+JOIN BOOK_SALES using (book_id)
+WHERE YEAR(sales_date) = 2022 and MONTH(sales_date) = 1
+GROUP BY category
+ORDER BY category
+
+-- 즐겨찾기가 가장 많은 식당 정보 출력하기
+SELECT food_type, rest_id, rest_name, favorites
+FROM (
+    SELECT *, 
+    RANK() OVER(PARTITION BY food_type ORDER BY favorites DESC) as r
+    FROM REST_INFO
+) AS T
+WHERE r = 1
+ORDER BY food_type DESC
+
+-- 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기
+-- 풀이 1
+SELECT DISTINCT CAR_ID,
+    IF (CAR_ID in (
+        SELECT CAR_ID
+        FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+        WHERE '22-10-16' BETWEEN START_DATE and END_DATE), '대여중', '대여 가능') as AVAILABILITY
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY   
+ORDER BY CAR_ID DESC
+
+-- 풀이 2
+SELECT CAR_ID,
+    IF(SUM(IF('2022-10-16' BETWEEN START_DATE and END_DATE, 1, 0)) = 1, '대여중', '대여 가능') as AVAILABILITY
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+ORDER BY CAR_ID DESC
--- SQL/[GROUP BY] LV4.sql
+++ SQL/[GROUP BY] LV4.sql
@@ -19,3 +19,13 @@ SELECT hour, COUNT(animal_id) as COUNT
 FROM HOUR_TABLE
 LEFT JOIN ANIMAL_OUTS on hour = hour(datetime)
 GROUP BY hour
+
+-- 식품분류별 가장 비싼 식품의 정보 조회하기
+SELECT category, price as MAX_PRICE, product_name
+FROM (
+    SELECT *, RANK() OVER (PARTITION BY category ORDER BY price DESC) as r
+    FROM FOOD_PRODUCT
+    WHERE category in ('과자', '국', '김치', '식용유')
+) as T
+WHERE r = 1
+ORDER BY price DESC
