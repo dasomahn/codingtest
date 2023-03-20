759a824ae78ff73892e8825da2210a794abfdd7d
Dasom Ahn <lizzy0521@naver.com>
2023-03-20 14:01:24 +0900

    20230320

--- Dev-Matching 기출/다단계 칫솔 판매.py
+++ Dev-Matching 기출/다단계 칫솔 판매.py
@@ -1,19 +1,20 @@
 def solution(enroll, referral, seller, amount):
-    dic = {d:0 for d in enroll}
+    dic = {d : 0 for d in enroll}
+    ref_dic = {r : referral[i] for i, r in enumerate(enroll)}
     
     for sell, a in zip(seller, amount):
-        money = a * 100
-        ref = referral[enroll.index(sell)]
-        if (ref == '-'):
-            dic[sell] += money
-        else:
-            while (ref != "-"):
-                if (money < 10):
-                    dic[ref] += money
-                    break
-                dic[sell] += money * 0.9
-                dic[ref] += money * 0.1
-                money *= 0.1
-                ref = referral[enroll.index(ref)]
-    
+        dic[sell] += a * 100 * 0.9
+        
+        money = a * 100 * 0.1
+        ref = ref_dic[sell]
+     
+        while (ref != "-"):
+            dic[ref] += money
+            if (money < 10):
+                break
+            money = int(money * 0.1)
+            dic[ref] -= money
+            
+            ref = ref_dic[ref]
+
     return list(dic.values())
