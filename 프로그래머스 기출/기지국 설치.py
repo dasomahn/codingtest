e2d387c941d580990aeece1e160943d459382188
Dasom Ahn <lizzy0521@naver.com>
2023-04-02 01:42:27 +0900

    20230402
    fixed

--- 프로그래머스 기출/기지국 설치.py
+++ 프로그래머스 기출/기지국 설치.py
@@ -1,22 +1,19 @@
 def solution(n, stations, w):
-    lst = [0] * (n+1)
-    
-    for s in stations:
-        for i in range(s-w, s+w+1):
-            if i > n: break
-            lst[i] = 1
+    wave = 2*w+1
     
     cnt = 0
-    for i in range(1, n+1):
-        if lst[i]:
+    prev = 1
+    for s in stations:
+        diff = max(1, s-w) - prev
+        if (diff <= 0):
+            prev = s+w+1
             continue
+            
+        cnt += -(-diff//wave)
+        prev = s+w+1
         
-        if i+w > n:
-            cnt += 1
-            break
-        for j in range(i, i+w*2+1):
-            if j > n: break
-            lst[j] = 1        
-        cnt += 1
-        
+    diff = n - prev + 1
+    if diff > 0:
+        cnt += -(-diff//wave)
+
     return cnt
