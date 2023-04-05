35c14b2b61406e56f6591e589f7f3cfa448cc852
Dasom Ahn <lizzy0521@naver.com>
2023-04-05 16:27:37 +0900

    20230405
    fix init, rotation

--- Baekjoon/17144.py
+++ Baekjoon/17144.py
@@ -1,28 +1,12 @@
 class Room:
-    def __init__(self, param):
-        self.r = int(param[0])
-        self.c = int(param[1])
-        self.t = int(param[2])
-        self.grid = []
+    def __init__(self):
+        self.r, self.c, self.t = map(int, input().split())
+        self.grid = [list(map(int, input().split())) for _ in range(self.r)]
 
-        self.up = -1
-        self.down = -1
-        self.total = 0
-
-    def set(self):
-        for row in range(self.r):
-            cols = list(map(int, input().split(" ")))
-            if cols[0] == -1:
-                if self.up == -1:
-                    self.up = row
-                else:
-                    self.down = row
-            self.grid.append(cols)
-            self.total += sum(cols)
-
-        self.total += 2
-        self.grid[self.up][0] = 0
-        self.grid[self.down][0] = 0
+        for r in range(self.r):
+            if self.grid[r][0] == -1:
+                self.up, self.down = r, r + 1
+                break
 
     def bound(self, x, y):
         if (0 <= x < self.r) and (0 <= y < self.c):
@@ -36,77 +20,50 @@ class Room:
         for x in range(self.r):
             for y in range(self.c):
                 div = self.grid[x][y] // 5
-                if div <= 0:
-                    continue
-
-                for dx, dy in dirs:
-                    nx, ny = x + dx, y + dy
-                    if self.bound(nx, ny):
-                        dust[nx][ny] += div
-                        self.grid[x][y] -= div
+                if div > 0:
+                    for dx, dy in dirs:
+                        nx, ny = x + dx, y + dy
+                        if self.bound(nx, ny):
+                            dust[nx][ny] += div
+                            self.grid[x][y] -= div
 
         for x in range(self.r):
             for y in range(self.c):
                 self.grid[x][y] += dust[x][y]
 
-    def cleanup(self):
-        w, h = self.c - 1, self.up
+    def clean(self, direction, x, y):
+        if x == self.up:
+            edge = 0
+        elif x == self.down:
+            edge = self.r - 1
+        edges = [(x, 0), (x, self.c - 1), (edge, 0), (edge, self.c - 1)]
 
-        # left
-        for x in range(h, 0, -1):
-            self.grid[x][0] = self.grid[x - 1][0]
+        for dx, dy in direction:
+            while True:
+                nx, ny = x + dx, y + dy
+                self.grid[x][y] = self.grid[nx][ny]
+                x, y = nx, ny
 
-        # upper
-        for y in range(w):
-            self.grid[0][y] = self.grid[0][y + 1]
-
-        # right
-        for x in range(h):
-            self.grid[x][w] = self.grid[x + 1][w]
-
-        # lower
-        for y in range(w, 1, -1):
-            self.grid[h][y] = self.grid[h][y - 1]
-
-    def cleandown(self):
-        w, h = self.c - 1, self.down
-
-        # left
-        for x in range(h, self.r - 1):
-            self.grid[x][0] = self.grid[x + 1][0]
-
-        # lower
-        for y in range(w):
-            self.grid[self.r - 1][y] = self.grid[self.r - 1][y + 1]
-
-        # right
-        for x in range(self.r - 1, h, -1):
-            self.grid[x][w] = self.grid[x - 1][w]
-
-        # upper
-        for y in range(w, 1, -1):
-            self.grid[h][y] = self.grid[h][y - 1]
+                if (nx, ny) in edges:
+                    break
 
     def monitor(self):
-        self.set()
-
-        for i in range(self.t):
+        for _ in range(self.t):
             self.diffusion()
 
-            self.cleanup()
-            self.cleandown()
+            self.clean(dirs, self.up, 0)
+            self.clean(c_dirs, self.down, 0)
 
-            self.total -= (self.grid[self.up][0] + self.grid[self.down][0])
             self.grid[self.up][0] = 0
             self.grid[self.up][1] = 0
             self.grid[self.down][0] = 0
             self.grid[self.down][1] = 0
 
+        total = sum(sum(self.grid[r]) for r in range(self.r))
+        return total
 
-dirs = ((1, 0), (0, 1), (-1, 0), (0, -1))
+dirs = ((-1, 0), (0, 1), (1, 0), (0, -1))
+c_dirs = ((1, 0), (0, 1), (-1, 0), (0, -1))
 
-param = input().split(" ")
-room = Room(param)
-room.monitor()
-
-print(room.total)
+room = Room()
+print(room.monitor())
