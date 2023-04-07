2881208d9922129e74b9141caeb52dcbb51317af
Dasom Ahn <lizzy0521@naver.com>
2023-04-08 00:34:19 +0900

    20230408 - 15650
    fixed

--- Baekjoon/15650.py
+++ Baekjoon/15650.py
@@ -23,13 +23,11 @@ def dfs(n, depth):
         print(' '.join(map(str, ans)))
     else:
         for i in range(n, N):
-            visited[i] = 1
             ans.append(i+1)
 
             dfs(i+1, depth+1)
 
             ans.pop()
-            visited[i] = 0
 
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
@@ -45,7 +43,6 @@ for test_case in range(1, T + 1):
     comb2(0, 0)
 
     # solution #3
-    visited = [0] * (N+1)
     ans = []
     dfs(0, 0)
     print(f"\n{test_case} end --------------------------------------------------\n")
