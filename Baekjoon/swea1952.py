b0a39156def9cd5694f98ed150f468efb37c521b
Dasom Ahn <lizzy0521@naver.com>
2023-04-09 00:13:15 +0900

    20230409 - swea1952
    DFS ver.

--- Baekjoon/swea1952.py
+++ Baekjoon/swea1952.py
@@ -2,12 +2,25 @@ import sys
 
 sys.stdin = open("swea1952input.txt", "r")
 
+
+def dfs(idx, cost):
+    global min_cost
+
+    if idx >= 12:
+        min_cost = min(min_cost, cost)
+    else:
+        dfs(idx + 1, cost + visit[idx] * day)
+        dfs(idx + 1, cost + month)
+        dfs(idx + 3, cost + quarter)
+
+
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     day, month, quarter, year = map(int, input().split())
     visit = list(map(int, input().split()))
 
+    # 풀이 1 DP
     dp = [min(visit[i] * day, month) for i in range(12)]
     dp[1] += dp[0]
     dp[2] = min(dp[1] + dp[2], quarter)
@@ -17,3 +30,9 @@ for test_case in range(1, T + 1):
         dp[i] = min(dp[i - 1] + dp[i], dp[i - 3] + quarter)
 
     print(f"#{test_case} {min(dp[11], year)}")
+
+    # 풀이 2 DFS
+    min_cost = year
+    dfs(0, 0)
+
+    print(f"#{test_case} {min_cost}")
