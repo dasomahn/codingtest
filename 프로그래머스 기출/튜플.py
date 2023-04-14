3dac843bd4b75512c121d087c0c6d68c10b8bfa7
Dasom Ahn <lizzy0521@naver.com>
2023-04-14 20:42:50 +0900

    20230414 - 튜플
    fixed

--- 프로그래머스 기출/튜플.py
+++ 프로그래머스 기출/튜플.py
@@ -1,16 +1,28 @@
+'''
 def solution(s):
     answer = []
     
     arr = s[2:-2].split("},{")
     arr.sort(key=lambda x:len(x))
     
-    for i, a in enumerate(arr):
-        arr[i] = list(map(int, a.split(",")))
-    
-    for lst in arr:
+    for a in arr:
+        lst = list(map(int, a.split(",")))
         for l in lst:
             if l not in answer:
                 answer.append(l)
                 break
     
     return answer
+'''
+
+# fixed
+def solution(s):
+    dic = {}
+
+    arr = s[2:-2].split("},{")
+    for a in arr:
+        lst = list(map(int, a.split(",")))
+        for l in lst:
+            dic[l] = dic.get(l, 0) + 1
+
+    return [k for k in sorted(dic.keys(), key=lambda x: dic[x], reverse=True)]
