8eb51213f5d24b6258bd20c460a8f83899e14289
Dasom Ahn <lizzy0521@naver.com>
2023-04-22 17:23:08 +0900

    20230422 - 두 큐 합 같게 만들기

--- 프로그래머스 기출/두 큐 합 같게 만들기.py
+++ 프로그래머스 기출/두 큐 합 같게 만들기.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 def solution(queue1, queue2):
     lst = queue1+queue2
     if sum(lst)%2:
@@ -8,18 +6,20 @@ def solution(queue1, queue2):
     n = len(lst)
     target = sum(lst)/2
     
-    q = deque([(0, n//2, 0, sum(queue1))])
+    s, e = 0, n//2
+    partsum = sum(queue1)
     
-    while (q):
-        s, e, moves, partsum = q.popleft()
-        
+    moves = 0
+    while (partsum != target):
         if moves > 1.5*n - 3:
             return -1
         
-        if partsum == target:
-            return moves
+        if partsum > target:
+            partsum -= lst[s]
+            s = (s+1)%n
         else:
-            if partsum > target:
-                q.append([(s+1)%n, e, moves+1, partsum-lst[s]])
-            else:
-                q.append([s, (e+1)%n, moves+1, partsum+lst[e]])
+            partsum += lst[e]
+            e = (e+1)%n
+        moves += 1
+                
+    return moves
